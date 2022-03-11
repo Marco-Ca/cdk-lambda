ce5e0c3d22344033529806fab1e0d29e69ce25e9
marco-ca <marco.castro@aller.com>
2022-03-11 11:43:52 +0200

    feat: dynamodb table

--- lib/cdk-serverless-lambda-stack.ts
+++ lib/cdk-serverless-lambda-stack.ts
@@ -1,18 +1,16 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigw from '@aws-cdk/aws-apigateway';
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
 
 export class CdkServerlessLambdaStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-
-    const queue = new sqs.Queue(this, 'CdkServerlessLambdaQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
-    });
-
-    const topic = new sns.Topic(this, 'CdkServerlessLambdaTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    super(scope, id, props);    
   }
+
+  // DynamoDB table definition
+  const table = new dynamodb.Table(this, 'Table', {
+    partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+    tableName: 'TableName',
+  });
 }
